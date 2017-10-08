472ca2d1e82d0b0a676900e9dd9e36f741148f1a
Helge Deller <deller@gmx.de>
2017-10-08 08:40:11 +0200

    Clean up boot order, better messages

--- src/parisc/parisc.c
+++ src/parisc/parisc.c
@@ -133,28 +133,28 @@ int parisc_boot_menu(unsigned char **iplstart, unsigned char **iplend)
 
 	/* seek to beginning of disc/CD */
 	ret = process_op(&disk_op);
-	printf("DISK_SEEK returned %d\n", ret);
-	// if (ret)
-	//	return 0;
+	// printf("DISK_SEEK returned %d\n", ret);
+	if (ret)
+		return 0;
 
 	/* read boot sector of disc/CD */
 	target[0] = 0xabcd;
 	disk_op.command = CMD_READ;
-	printf("blocksize is %d\n", disk_op.drive_fl->blksize);
+	// printf("blocksize is %d\n", disk_op.drive_fl->blksize);
 	disk_op.count = (FW_BLOCKSIZE / disk_op.drive_fl->blksize);
 	disk_op.lba = 0;
 	ret = process_op(&disk_op);
-	printf("DISK_READ(count=%d) = %d\n", disk_op.count, ret);
-	//if (ret)
-	//	return 0;
-	//
+	// printf("DISK_READ(count=%d) = %d\n", disk_op.count, ret);
+	if (ret)
+		return 0;
+
 	unsigned int ipl_addr = be32_to_cpu(target[0xf0/sizeof(int)]); /* offset 0xf0 in bootblock */
 	unsigned int ipl_size = be32_to_cpu(target[0xf4/sizeof(int)]);
 	unsigned int ipl_entry= be32_to_cpu(target[0xf8/sizeof(int)]);
 
 	/* check LIF header of bootblock */
-	printf("boot magic is 0x%x (should be 0x8000)\n", target[0]>>16);
-	printf("ipl  start at 0x%x, size %d, entry 0x%x\n", ipl_addr, ipl_size, ipl_entry);
+	// printf("boot magic is 0x%x (should be 0x8000)\n", target[0]>>16);
+	// printf("ipl  start at 0x%x, size %d, entry 0x%x\n", ipl_addr, ipl_size, ipl_entry);
 
 	// TODO: check ipl values, out of range, ... ?
 
@@ -163,16 +163,16 @@ int parisc_boot_menu(unsigned char **iplstart, unsigned char **iplend)
 	disk_op.count = 0; // (ipl_size / disk_op.drive_fl->blksize);
 	disk_op.lba = (ipl_addr / disk_op.drive_fl->blksize);
 	ret = process_op(&disk_op);
-	printf("DISK_SEEK to IPL returned %d\n", ret);
+	// printf("DISK_SEEK to IPL returned %d\n", ret);
 
 	/* read IPL */
 	disk_op.command = CMD_READ;
 	disk_op.count = (ipl_size / disk_op.drive_fl->blksize);
 	disk_op.lba = (ipl_addr / disk_op.drive_fl->blksize);
 	ret = process_op(&disk_op);
-	printf("DISK_READ IPL returned %d\n", ret);
+	// printf("DISK_READ IPL returned %d\n", ret);
 
-	printf("First word at %p is 0x%x\n", target, target[0]);
+	// printf("First word at %p is 0x%x\n", target, target[0]);
 
 	/* execute IPL */
 	// TODO: flush D- and I-cache, not needed in emulation ?
@@ -220,10 +220,11 @@ void __VISIBLE start_parisc_firmware(unsigned long ram_size,
 	unsigned char *iplstart, *iplend;
 
 	/* Initialize PAGE0 */
+	memset((void*)PAGE0, 0, sizeof(*PAGE0));
 	PAGE0->memc_cont = ram_size;
 	PAGE0->memc_phsize = ram_size;
 	PAGE0->mem_free = 4*4096; // 16k ??
-	PAGE0->mem_hpa = 0; // /* HPA of the boot-CPU */
+	PAGE0->mem_hpa = CPU_HPA; // /* HPA of boot-CPU */
 	PAGE0->mem_pdc = (unsigned long) &pdc_entry;
 	PAGE0->mem_10msec = CPU_CLOCK_MHZ*(1000000ULL/100);
 
@@ -238,14 +239,16 @@ void __VISIBLE start_parisc_firmware(unsigned long ram_size,
 	DebugOutputPort = PORT_SERIAL1;
 	// PlatformRunningOn = PF_QEMU;  // emulate runningOnQEMU()
 
-	printf("\n");
-	printf("PARISC SeaBIOS Firmware started, %lu MB RAM.\n", ram_size/1024/1024);
-
 	cpu_hz = 100 * PAGE0->mem_10msec; /* Hz of this PARISC */
-	printf("1 CPU at %d.%06d MHz\n",
-			cpu_hz / 1000000, cpu_hz % 1000000 );
+	printf("\n");
+	printf("PARISC SeaBIOS Firmware, 1 x PA7300LC (PCX-L2) at %d.%06d MHz, %lu MB RAM.\n",
+		cpu_hz / 1000000, cpu_hz % 1000000,
+		ram_size/1024/1024);
 
 	// mdelay(1000); // test: "wait 1 second"
+	// test endianess functions
+	// printf("0xdeadbeef %x %x\n", cpu_to_le16(0xdeadbeef),cpu_to_le32(0xdeadbeef));
+	// printf("0xdeadbeef %x %x\n", le16_to_cpu(0xefbe),le32_to_cpu(0xefbeadde));
 
 	// handle_post();
 	serial_debug_preinit();
@@ -256,24 +259,28 @@ void __VISIBLE start_parisc_firmware(unsigned long ram_size,
 
 	serial_setup();
 	ata_setup();
-//
-//	printf("0xdeadbeef %x %x\n", cpu_to_le16(0xdeadbeef),cpu_to_le32(0xdeadbeef));
-//	printf("0xdeadbeef %x %x\n", le16_to_cpu(0xefbe),le32_to_cpu(0xefbeadde));
 
-	if (parisc_boot_menu(&iplstart, &iplend)) {
-		void (*start_ipl)(long interactive, long mem_free);
+	printf("\n");
 
-		start_ipl = (void *) iplstart;
-		start_ipl(1, (long)iplend);
-		hlt(); /* this ends the emulator */
-	}
-
+	/* directly start Linux kernel if it was given on qemu command line. */
 	if (linux_kernel_entry) {
 		void (*start_kernel)(unsigned long mem_free, unsigned long cmdline,
 			unsigned long rdstart, unsigned long rdend);
 
+		printf("Starting Linux kernel which was loaded by qemu.\n\n");
 		start_kernel = (void *) linux_kernel_entry;
 		start_kernel(PAGE0->mem_free, cmdline, initrd_start, initrd_end);
+		hlt(); /* this ends the emulator */
+	}
+
+	/* check for bootable drives, and load and start IPL bootloader if possible */
+	if (parisc_boot_menu(&iplstart, &iplend)) {
+		void (*start_ipl)(long interactive, long mem_free);
+
+		printf("Starting IPL boot code from boot medium.\n\n");
+		start_ipl = (void *) iplstart;
+		start_ipl(1, (long)iplend);
+		hlt(); /* this ends the emulator */
 	}
 
 	hlt(); /* this ends the emulator */
