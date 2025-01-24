hdeller/seabios-hppa

commit 8d6b0df62d3709e3541a06ec8f4d8311235f3654

parisc: Fix return value of PDC_CACHE/PDC_CACHE_RET_SPID for space id hashing
HP-UX 11 64-bit uses space ID hashing.
If qemu implements the diagnose registers and mfdiag/mtdiag instructions
(qemu > v9.0 required), allow PDC to ask the CPU and return the correct
value of space id hashing bits.

Signed-off-by: Helge Deller <deller@gmx.de>
Suggested-by: Sven Schnelle <svens@stackframe.org>

src/parisc/parisc.c
@@ -165,6 +165,7 @@ extern char pdc_entry_table_end;
 extern char iodc_entry[512];
 extern char iodc_entry_table;
 extern char iodc_entry_table_one_entry;
+extern long sr_hashing_enabled(void);
 
 /* args as handed over for firmware calls */
 #define ARG0 arg[7-0]
@@ -1656,9 +1657,18 @@ static int pdc_cache(unsigned long *arg)
 
             memcpy(result, machine_cache_info, sizeof(*machine_cache_info));
             return PDC_OK;
-        case PDC_CACHE_RET_SPID:	/* returns space-ID bits when sr-hasing is enabled */
-            memset(result, 0, 32 * sizeof(unsigned long));
-            result[0] = 0;
+        case PDC_CACHE_RET_SPID:
+            /*
+             * Return space-ID bits when space register hashing is enabled.
+             * The Linux kernel disables sr-hashing, while HP-UX 11 (64-bit) uses hashing.
+             * For details check the assembly in arch/parisc/kernel/pacache.S in the Linux kernel
+             * and read some analysis here:
+             * https://patchwork.ozlabs.org/project/qemu-devel/patch/20240324080945.991100-3-svens@stackframe.org/#3289160
+             */
+            if (sr_hashing_enabled() == 0)
+                result[0] = 0;
+            else /* when HPPA64_DIAG_SPHASH_ENABLE bit is set: */
+                result[0] = HPPA64_PDC_CACHE_RET_SPID_VAL;
             return PDC_OK;
     }
     dprintf(0, "\n\nSeaBIOS: Unimplemented PDC_CACHE function %ld %lx %lx %lx %lx\n", ARG1, ARG2, ARG3, ARG4, ARG5);

src/parisc/head.S
@@ -481,6 +481,26 @@ ENTRY(firmware_default_ivt)	/* to detect crashes */
 	.endr
 END(smp_ivt)
 
+/*******************************************************
+ Check if space register hashing is enabled.
+ Returns non-zero if sr-hashing in CPU is enabled.
+ Needs adjustment when we emulate PCX-T later.
+ Requires at least qemu v9.x
+ *******************************************************/
+ENTRY(sr_hashing_enabled)
+	copy %r0,%r26
+	copy %r0,%r28		/* zero-initialize %r28 in case qemu can't emulate mfdiag call */
+#ifdef CONFIG_64BIT
+	.word 0x144008bc	/* PCXU,PCXU+,PCXW,PCXW+,PCXW2: mfdiag %dr2, %r28 */
+	depdi 1,54,1, %r26	/* set DIAG_SPHASH_ENABLE (bit 54) of %dr2 */
+#else
+	.word 0x141c0600	/* PCXL: mfdiag %dr0, %r28 */
+	depwi 3,28,2, %r26	/* Set DHASH_EN & IHASH_EN of %dr0 */
+#endif
+	bv    %r0(%rp)
+	and   %r28, %r26, %r28	/* mask hash enable bits */
+END(sr_hashing_enabled)
+
 
 /*******************************************************
 	PDC and IODC entry

src/parisc/hppa_hardware.h
@@ -79,4 +79,8 @@
 #define LMMIO_DIRECT0_MASK  0x308
 #define LMMIO_DIRECT0_ROUTE 0x310
 
+/* space register hashing */
+#define HPPA64_DIAG_SPHASH_ENABLE       0x200   /* DIAG_SPHASH_ENAB (bit 54) */
+#define HPPA64_PDC_CACHE_RET_SPID_VAL   0xfe0   /* PDC return value on 64-bit CPU */
+
 #endif

commit 650b5494a12351207c49006a2ce5e2c61771dab3

parisc: Fix PDC_CACHE/PDC_CACHE_RET_SPID return value
In case space register hashing is enabled, return 0xfe00000 instead of 0xfe0.

Fixes: 8d6b0df62d37 ("parisc: Fix return value of PDC_CACHE/PDC_CACHE_RET_SPID for space id hashing")
Signed-off-by: Helge Deller <deller@gmx.de>

src/parisc/parisc.c
@@ -1674,7 +1674,7 @@ static int pdc_cache(unsigned long *arg)
             if (sr_hashing_enabled() == 0)
                 result[0] = 0;
             else /* when HPPA64_DIAG_SPHASH_ENABLE bit is set: */
-                result[0] = HPPA64_PDC_CACHE_RET_SPID_VAL;
+                result[0] = HPPA64_PDC_CACHE_RET_SPID_VAL << 16;
             return PDC_OK;
     }
     dprintf(0, "\n\nSeaBIOS: Unimplemented PDC_CACHE function %ld %lx %lx %lx %lx\n", ARG1, ARG2, ARG3, ARG4, ARG5);